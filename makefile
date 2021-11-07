20e02e69b04346f5595c8a56f47295635296c9ef
Accacio <raccacio@poli.ufrj.br>
2021-11-07 01:40:56 +0100

    use inotify to recompile control.c

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 # [[file:Readme.org::*Testing][Testing:1]]
 # Pendulum
 compile:
-	gcc src/pendulum.c -Os -lncursesw -I/usr/include/ncursesw -lm -ltcc -o pendulum -ldl
+	gcc -g src/pendulum.c -Os -lncursesw -I/usr/include/ncursesw -lm -ltcc -o pendulum -ldl -lpthread
 
 run:
 	@./pendulum
--- src/pendulum.c
+++ src/pendulum.c
@@ -1,7 +1,10 @@
 /* [[file:../Readme.org::*The Code][The Code:1]] */
 #include <stdlib.h>
 #include <math.h>
+#include <unistd.h>
 #include <sys/time.h>
+#include <poll.h>
+#include <sys/inotify.h>
 #include <curses.h>
 #include <locale.h>
 #include <libtcc.h>
@@ -14,6 +17,9 @@
 #define PI 3.14
 #define deg(x) x/180.*PI
 
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define BUF_LEN     ( 1024 *( EVENT_SIZE + 16 ) )
+
 char preamble[] = "#include <tcclib.h>\n"
     "#include <math.h>\n"
     "#define PI 3.14\n"
@@ -21,12 +27,10 @@ char preamble[] = "#include <tcclib.h>\n"
     "typedef struct{\n"
     "    double l,M,m,d;\n"
     "} Sys;\n"
-
-    "typedef struct{"
-    "double x, dx, a, da;"
-    "} State;"
-
-    "double control(State state,uint8_t size,Sys sys,double u)\n"
+    "typedef struct{\n"
+    "double x, dx, a, da;\n"
+    "} State;\n"
+    "double control(State state,Sys sys,double u)\n"
     "{\n"
     ;
 char postamble[] = "return u;\n"
@@ -84,7 +88,7 @@ draw(State state,Sys sys,double u){
     /* ground */
     move((int) y+2,0);
     hline(ACS_HLINE, mx);
-    for(float i=-2;i<2;i+=0.5){
+    for(double i=-2;i<2;i+=0.5){
         mvprintw(y+3,mx/4*i+mx/2,"|",i);
         mvprintw(y+4,mx/4*i+mx/2,"%.2f",i);
     }
@@ -97,7 +101,7 @@ draw(State state,Sys sys,double u){
     mvprintw(y+1,x-4,"└o-----o┘");
 
     /* rod */
-    for(float i = 0.1;i<1;i+=0.01){
+    for(double i = 0.1;i<1;i+=0.01){
         double absfa=fabsf(state.a);
         mvprintw(floor(y+i*l*ca),floor(x+i*l*sa),"|");
        if(sa<0&&ca>0.5) mvprintw(floor(y+i*l*ca),floor(x+i*l*sa),"/");
@@ -149,10 +153,8 @@ physics(State * state,Sys sys,double dt,double u) {
 }
 /* Physics Simulation:3 ends here */
 
-/* [[file:../Readme.org::*Main Loop][Main Loop:1]] */
-main(int c, char **v){
-    TCCState *tccState;
-    char source_file[10] = "control.c";
+
+char* get_fileChars(char* source_file){
     FILE *source;
     int fileSize;
     char *fileChars;
@@ -163,28 +165,68 @@ main(int c, char **v){
     }
     fseek(source,0,SEEK_END);
     fileSize=ftell(source);
-    fileChars = (char*) realloc(fileChars,sizeof(char)*sizeof(postamble)+sizeof(char)*sizeof(preamble)+sizeof(char)*fileSize+1);
-    strcpy(fileChars, preamble);
+    int newSize=sizeof(char)*sizeof(postamble)+sizeof(char)*sizeof(preamble)+sizeof(char)*fileSize+1;
+    fileChars = (char*) malloc(newSize);
+    memset(fileChars, '\0', newSize);
+    strncpy(fileChars, preamble,newSize-1);
     fseek(source,0,SEEK_SET);
     fread((char*) fileChars+sizeof(preamble)-1,fileSize,1,source);
     strcat((char*)fileChars,postamble);
     fclose(source);
+    return (char*) fileChars;
+}
 
-    tccState = tcc_new();
-    tcc_set_output_type(tccState, TCC_OUTPUT_MEMORY);
+/* [[file:../Readme.org::*Main Loop][Main Loop:1]] */
+int main(int c, char **v){
+    /* lua_State* luaState = luaL_newstate(); */
+    /* luaL_openlibs(luaState); */
+
+    /* get default locale */
+    char * locale = setlocale(LC_ALL, 0);
+    setlocale(LC_ALL, "");
+
+    TCCState *tccState;
+    TCCState *tempTccState;
+
+
+    int length, i = 0;
+    int fd;
+    char wdbuffer[BUF_LEN];
+    fd = inotify_init1(IN_NONBLOCK);
+    if ( fd <0)
+    {
+        fprintf(stderr,"inotify_init");
+        exit(EXIT_FAILURE);
+    }
+
+    char source_file[10] = "control.c";
+
+    /* Add watch */
+    int control_watch = inotify_add_watch( fd, source_file, IN_MODIFY);
+    if (control_watch==-1) {
+        fprintf(stderr,"Could not add watch");
+        exit(EXIT_FAILURE);
+    }
+    struct pollfd pfd = { fd, POLLIN, 0 };
+    char *fileChars;
 
     double (*control)(State, Sys,double)= 0;
-
+    double (*temp_control)(State, Sys,double)= 0;
     if(!control){
+        setlocale(LC_ALL, locale);
+        fileChars = get_fileChars(source_file);
+        tccState = tcc_new();
+        tcc_set_output_type(tccState, TCC_OUTPUT_MEMORY);
         if(tcc_compile_string(tccState, fileChars)<0){
             exit(EXIT_FAILURE);
         };
+        free(fileChars);
         tcc_relocate(tccState, TCC_RELOCATE_AUTO);
-        control = tcc_get_symbol(tccState, "control");
+        control = (double (*) (State,Sys,double)) tcc_get_symbol(tccState, "control");
+        setlocale(LC_ALL, "");
     }
 
 
-    setlocale(LC_ALL, "");
 
     uint8_t size = 4;
     Sys sys = {2,5,1,1}; // l M m d
@@ -203,20 +245,56 @@ main(int c, char **v){
         return -1;
     }
     for(;;){
+        /* Verify if control was changed */
+        int ret = poll(&pfd,1,5);
+        if (ret > 0)
+        {
+            length = read(fd, wdbuffer, sizeof wdbuffer);
+            if (length>0)
+            {
+                struct inotify_event *watcher_event = ( struct inotify_event * ) &wdbuffer[ i ];
+                if ( watcher_event->mask & IN_MODIFY )
+                {
+                    /* mvprintw(4, 3, "File changed"); */
+                    if (watcher_event->wd==control_watch) {
+                        setlocale(LC_ALL, locale);
+                        fileChars = get_fileChars(source_file);
+                        tempTccState = tcc_new();
+                        tcc_set_output_type(tempTccState, TCC_OUTPUT_MEMORY);
+                        if(tcc_compile_string(tempTccState, fileChars)<0){
+                            tcc_delete(tempTccState);
+                            control = (double (*) (State,Sys,double)) tcc_get_symbol(tccState, "control");
+                            /* exit(EXIT_FAILURE); */
+                        }
+                        else
+                        {
+                            tcc_delete(tccState);
+                            tcc_relocate(tempTccState, TCC_RELOCATE_AUTO);
+                            control = (double (*) (State,Sys,double)) tcc_get_symbol(tempTccState, "control");
+                            tccState = tempTccState;
+                        }
+                        setlocale(LC_ALL, "");
+
+                    }
+                }
+            }
+        }
+
         ch = getch();
         if (ch!=ERR){
             if(ch==68){state.dx-=1;} // nudge left
             if(ch==67){state.dx+=1;} // nudge right
             if(ch==65){
+                u=0;
                 sInit[0] =0;
                 sInit[1] =0;
                 sInit[2] =160./180*PI;
                 sInit[3] =0;
             }
             if(ch==66){
-                sInit[0] =0;
+                sInit[0] =-1.5;
                 sInit[1] =0;
-                sInit[2] =30.0/180*PI;
+                sInit[2] =20.0/180*PI;
                 sInit[3] =0;
             }
             if(ch==10){
